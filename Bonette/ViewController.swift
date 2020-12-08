a08915bbc77b04df7c554fda0c8869c235000a4a
IGP <pyltsov@gmail.com>
2020-12-08 11:43:43 +0100

    Initial Commit

---
+++ Bonette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bonette
+//
+//  Created by ИГОРЬ on 08/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
