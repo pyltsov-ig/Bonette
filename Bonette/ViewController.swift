20fd288ad91638d554f21aa1532de3c389b585eb
IGP <pyltsov@gmail.com>
2020-12-10 13:51:20 +0100

    Это для продолжения работы с приложением

--- Bonette/ViewController.swift
+++ Bonette/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mySlider: UISlider!
     
+    @IBOutlet weak var myLabel: UILabel!
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         myBlueView.backgroundColor = .magenta
@@ -25,6 +29,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func myButtonClick(_ sender: UIButton) {
+        myLabel.text = "Мы кликнули !!!!!"
+        myButton.backgroundColor = .green
+    }
+    
+    @IBAction func mySliderMove(_ sender: UISlider) {
+        myLabel.text = String(mySlider.value)
+    }
+    
 }
 
