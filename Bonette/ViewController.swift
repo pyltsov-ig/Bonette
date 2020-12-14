5208a03a914f007ff59a6f9d23ad02da97a86b4c
IGP <pyltsov@gmail.com>
2020-12-14 11:56:22 +0100

    Для констрейнтов

--- Bonette/ViewController.swift
+++ Bonette/ViewController.swift
@@ -9,33 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var myBlueView: UIView!
-    
-    @IBOutlet weak var myButton: UIButton!
-    
-    @IBOutlet weak var mySlider: UISlider!
-    
-    @IBOutlet weak var myLabel: UILabel!
-    
-   
+       
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        myBlueView.backgroundColor = .magenta
-        myButton.setTitleColor(.black, for: UIControl.State.normal)
-        //self.delete(mySlider)
+       //self.delete(mySlider)
         
         
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func myButtonClick(_ sender: UIButton) {
-        myLabel.text = "Мы кликнули !!!!!"
-        myButton.backgroundColor = .green
-    }
-    
-    @IBAction func mySliderMove(_ sender: UISlider) {
-        myLabel.text = String(mySlider.value)
     }
     
 }
